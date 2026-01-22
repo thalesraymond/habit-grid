37938c5eb84b63001b4431484ce2c3d49b858ac2
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2026-01-21 23:22:43 -0300

    fix: file missing on last commit

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -3,6 +3,25 @@ import React from 'react';
 
 import HomeScreen from '../app/(tabs)/index';
 
+jest.mock('expo-router', () => {
+  const React = require('react');
+  const View = require('react-native').View;
+  
+  const Link = (props: any) => {
+    return React.createElement(View, props, props.children);
+  };
+  
+  Link.Trigger = ({ children }: any) => React.createElement(View, {}, children);
+  Link.Preview = () => null;
+  Link.Menu = ({ children }: any) => React.createElement(View, {}, children);
+  Link.MenuAction = () => null;
+  
+  return {
+    Link,
+  };
+});
+
+
 describe('HomeScreen', () => {
   it('renders successfully', () => {
     render(<HomeScreen />);
